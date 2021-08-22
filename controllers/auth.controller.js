7abcbfff46a2d71eb636c0fa83e72c38e20e1c5b
goncalveshigino <goncalveshigino20@gmail.com>
2021-08-22 14:19:04 +0100

    JWT

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,56 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator')
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt')
 
 
 
-const createUser = (req, res = response) => {
+
+const createUser = async (req, res = response) => {
 
     const { name, email, password } = req.body;
-        
-    return res.json({
-        ok: true,
-        msg: 'Criar usuario'
-    });
 
+ 
+        
+    try {
+         
+      //Verificar se naoa existe o email
+        const user = await User.findOne({ email: email });
+        
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Este email ja existe'
+            })
+        }
+
+        const dbUser = new User(req.body);
+        
+        //Hashear a senha
+        const salt = bcrypt.genSaltSync();
+        dbUser.password = bcrypt.hashSync(password, salt);
+
+        //Generar el JWT
+         const token = await generarJWT( dbUser.id, name );
+        //Criar usuario de DB
+        dbUser.save();
+        
+        //Generar resposta
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+            token
+        });
+         
+     } catch (error) {
+
+      return res.json({
+        ok: false,
+        msg: 'Criar usuario'
+      });
+
+     }
 }
 
 
